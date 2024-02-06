d2103aec52fbb57797063ec87b9016e18d1da6f2
Jonas Jakobson <jonas.jakobson@gmail.com>
2024-02-06 15:12:59 +0100

    sent file to '/'

--- myApp.js
+++ myApp.js
@@ -3,8 +3,13 @@ let app = express();
 
 console.log("Hello World");
 //First endpoint
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.send('Hello Express')
+}); */
+
+app.get("/", (req, res) => {
+  const absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
 });
 
 
