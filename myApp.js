9e35f97ebcd98149715539ccbb00eb3ac9599b4a
Jonas Jakobson <jonas.jakobson@gmail.com>
2024-02-07 08:39:26 +0100

    echo param

--- myApp.js
+++ myApp.js
@@ -40,6 +40,10 @@ app.get("/now", (req, res, next) => {
   res.send({time: req.time})
 })
 
+app.get("/:word/echo", (req, res) => {
+  const echoWord = req.params.word;
+  res.send({ "echo" : echoWord});
+})
 
 
 
